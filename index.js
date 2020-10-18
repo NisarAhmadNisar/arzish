a295eab51f1b291c71e8e459d3cd702493c3086f
NisarAhmadNisar <nisarahmadnisar05@gmail.com>
2020-10-18 23:37:52 +0530

    new try

--- index.js
+++ index.js
@@ -149,9 +149,16 @@ app.get("/api/products", async (req, res) => {
 //      res.sendFile(path.join(__dirname + "/frontend/build/index.html"));
 //    });
 
-app.get("/*", (req, res) =>
-  res.sendFile(path.join(__dirname + "/frontend/build/index.html"))
-);
+// app.get("/*", (req, res) =>
+//   res.sendFile(path.join(__dirname + "/frontend/build/index.html"))
+// );
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendfile(path.join((__dirname = "frontend/build/index.html")));
+  });
+}
 
 // Finished Setup
 app.listen(port, () =>
