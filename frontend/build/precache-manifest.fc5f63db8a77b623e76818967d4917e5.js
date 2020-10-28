361fac02eed512a798bf3a3f7485502657f221e4
NisarAhmadNisar <nisarahmadnisar05@gmail.com>
2020-10-28 16:42:12 +0530

    express deploy

---
+++ frontend/build/precache-manifest.fc5f63db8a77b623e76818967d4917e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2cbca906dcdaa0b37deb0a09bca64c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f65db952e64059670a7a",
+    "url": "/static/css/2.519f646e.chunk.css"
+  },
+  {
+    "revision": "7c37a30ca93ab06423a5",
+    "url": "/static/css/main.6aef7f8f.chunk.css"
+  },
+  {
+    "revision": "f65db952e64059670a7a",
+    "url": "/static/js/2.634d1e04.chunk.js"
+  },
+  {
+    "revision": "fa8eb3b5bf1814bc4eca09ef0104fc59",
+    "url": "/static/js/2.634d1e04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c37a30ca93ab06423a5",
+    "url": "/static/js/main.7b2a52a8.chunk.js"
+  },
+  {
+    "revision": "68a9665f6992de0c4239",
+    "url": "/static/js/runtime-main.fc75219d.js"
+  }
+]);
