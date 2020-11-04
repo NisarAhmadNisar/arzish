fc641d956da0f6ae0cae8bcd837fa0d8a688cb61
Nisar Ahmad <66984540+NisarAhmadNisar@users.noreply.github.com>
2020-11-04 16:46:07 +0530

    Update apolloClient.js

--- frontend/src/utils/apolloClient.js
+++ frontend/src/utils/apolloClient.js
@@ -1,7 +1,7 @@
 import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
-  playground: true,
+  
   uri:
     `${process.env.STRAPI_API_URL}/graphql` ||
     "http://localhost:1337/graphql"
